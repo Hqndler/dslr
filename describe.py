e822403513195558c12523c9f2d5ca048eb03c02
Handler <handler2104@gmail.com>
2024-11-25 16:31:23 +0100

    describe bonus

--- describe.py
+++ describe.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from math import sqrt
+from math import sqrt, isnan
 import sys, os
 
 class Describe():
@@ -31,6 +31,32 @@ class Describe():
             return
         return lst[(len(lst) // 4) * int(q)]
 
+    @classmethod
+    def unique(cls, lst):
+        return len(set(lst))
+
+    @classmethod
+    def top(cls, lst):
+        seen = set()
+        dups = dict()
+
+        for x in lst:
+            if x not in seen:
+                seen.add(x)
+                continue
+            if x in dups:
+                dups[x] += 1
+                continue
+            dups[x] = 1
+
+        if dups:
+            return max(dups, key=dups.get)
+        return list(seen)[0]
+
+    @classmethod
+    def freq(cls, lst):
+        return lst.count(Describe.top(lst))
+
 def main():
     if not os.path.exists(sys.argv[1]):
         print(f'"{sys.argv[1]}" no such file or directory.')
@@ -44,7 +70,7 @@ def main():
             if col.strip() == "Best Hand":
                 check = True
             continue
-        data[col] = sorted([float(x) for x in csv[col] if pd.notnull(x)])
+        data[col] = sorted([x for x in csv[col] if not isnan(x)])
 
     desc = {
         "Count" : [Describe.count(data[i]) for i in data],
@@ -57,10 +83,20 @@ def main():
         "Max"   : [Describe.max(data[i]) for i in data],
     }
 
+    bonus = {
+        "Unique" : [Describe.unique(data[i]) for i in data],
+        "Top"    : [Describe.top(data[i]) for i in data],
+        "Freq"   : [Describe.freq(data[i]) for i in data],
+    }
+
     print(" " * 8 + "".join([f"{i[:13]:>14}" for i in data]))
     for i in desc:
         print(f"{i:<8}" + "".join([f"{elem:14.6f}" for elem in desc[i]]))
 
+    if "bonus" in sys.argv: # no need to use argparse
+        for i in bonus:
+            print(f"{i:<8}" + "".join([f"{elem:14.6f}" for elem in bonus[i]]))
+
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
+    if len(sys.argv) >= 2:
         main()
